6bb0889770625d658c5e56a53b9c690cae734af3
chelsea zhai <chelsea.zhai@gmail.com>
2012-06-19 18:08:52 +0800

    update contact bean attributes

--- CommonToolkit/DataStruct/ContactBean.m
+++ CommonToolkit/DataStruct/ContactBean.m
@@ -13,8 +13,10 @@
 @implementation ContactBean
 
 @synthesize id = _id;
-@synthesize group = _group;
-@synthesize fullName = _fullName;
+@synthesize groups = _groups;
+@synthesize displayName = _displayName;
+@synthesize fullNames = _fullNames;
+@synthesize namePhonetics = _namePhonetics;
 @synthesize phoneNumbers = _phoneNumbers;
 @synthesize photo = _photo;
 
@@ -30,7 +32,7 @@
 }
 
 - (NSString *)description{
-    return [NSString stringWithFormat:@"ContactBean description: id = %d, group name = %@, full name = %@, phone numbers array = %@ an photo = %@, extension dictionary = %@", _id, _group, _fullName, _phoneNumbers, _photo, _extensionDic];
+    return [NSString stringWithFormat:@"ContactBean description: id = %d, group array = %@, display name = %@, full name array = %@, name phonetic array = %@, phone number array = %@ an photo = %@, extension dictionary = %@", _id, _groups, _displayName, _fullNames, _namePhonetics, _phoneNumbers, _photo, _extensionDic];
 }
 
 @end
